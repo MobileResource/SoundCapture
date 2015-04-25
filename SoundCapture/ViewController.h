8de653524013133131892305e1ad6a294da9d84d
choe <choe@choes-Mac.local>
2015-04-25 08:01:28 -0700

    Initial Commit

---
+++ SoundCapture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundCapture
+//
+//  Created by choe on 4/25/15.
+//  Copyright (c) 2015 bong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SoundCaptureTests/SoundCaptureTests.m
@@ -0,0 +1,40 @@
+//
+//  SoundCaptureTests.m
+//  SoundCaptureTests
+//
+//  Created by choe on 4/25/15.
+//  Copyright (c) 2015 bong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SoundCaptureTests : XCTestCase
+
+@end
+
+@implementation SoundCaptureTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
